8c00dc90753935a6859f2dd1621e5925c5af8787
David Chaves Rodríguez <A22DavidCR@iessanclemente.net>
2024-03-08 11:35:17 +0100

    priba

--- restore.sh
+++ restore.sh
@@ -1,3 +1,3 @@
 ##### restore.sh
 #!/bin/bash
-docker compose exec bd bash -c "mysql -u user -p database < /backup/backup.sql"
+docker compose exec bd bash -c "mysql -u root -p database < bd/backup.sql"
